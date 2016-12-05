19a986fe85c9794bdaeee9ce21af1ce45a83dc4c
Ken Siu <KenSiu@Kens-MacBook-Pro.local>
2016-12-05 15:47:27 +0800

    implement drawing

---
+++ Brush/ColorScheme.swift
@@ -0,0 +1,34 @@
+//
+//  ColorScheme.swift
+//  Brush
+//
+//  Created by Ken Siu on 5/12/2016.
+//  Copyright © 2016 Ken Siu. All rights reserved.
+//
+
+import UIKit
+
+class ColorScheme {
+    
+    static let Red = genColor(red: 213, green: 39, blue: 51)
+    static let Orange = genColor(red: 253, green: 164, blue: 41)
+    static let Yellow = genColor(red: 254, green: 217, blue: 83)
+    static let Green = genColor(red: 148, green: 217, blue: 103)
+    static let Blue = genColor(red: 27, green: 118, blue: 236)
+    static let Purple = genColor(red: 147, green: 71, blue: 194)
+    static let Black = genColor(red: 0, green: 0, blue: 0)
+    
+    static var Random: UIColor {
+        get {
+            let red = CGFloat(arc4random_uniform(255)) / 255.0
+            let green = CGFloat(arc4random_uniform(255)) / 255.0
+            let blue = CGFloat(arc4random_uniform(255)) / 255.0
+            
+            return genColor(red: red, green: green, blue: blue)
+        }
+    }
+    
+    static private func genColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: 1.0)
+    }
+}
--- Brush/DrawingViewController.swift
+++ Brush/DrawingViewController.swift
@@ -10,16 +10,107 @@ import UIKit
 
 class DrawingViewController: UIViewController {
 
+    @IBOutlet weak var drawingImageView: UIImageView!
+    @IBOutlet weak var topStackView: UIStackView!
+    
+    private var currentColor: UIColor = ColorScheme.Black
+    private var currentBrushSize: CGFloat = 10.0
+    private var firstPoint = CGPoint(x: 0, y: 0)
+    private var secondPoint = CGPoint(x: 0, y: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: touches method
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("began")
+        UIView.animate(withDuration: 0.5) {
+            self.topStackView.layer.opacity = 0.0
+        }
+        
+        if let touch = touches.first {
+            firstPoint = touch.location(in: drawingImageView)
+            drawALine(first: firstPoint, second: firstPoint)
+        }
+        
     }
-
-
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("moved")
+        if let touch = touches.first {
+            secondPoint = touch.location(in: drawingImageView)
+            drawALine(first: firstPoint, second: secondPoint)
+            firstPoint = secondPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("end")
+        if let touch = touches.first {
+            secondPoint = touch.location(in: drawingImageView)
+            drawALine(first: firstPoint, second: secondPoint)
+            firstPoint = secondPoint
+        }
+        
+        UIView.animate(withDuration: 0.5) {
+            self.topStackView.layer.opacity = 1.0
+        }
+    }
+    
+    // MARK: Customized Methods
+    @IBAction func moreTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Save or Share", message: "", preferredStyle: .actionSheet)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {(alert :UIAlertAction!) in
+            self.saveMasterPiece()
+        })
+        alertController.addAction(saveAction)
+        
+        let shareAction = UIAlertAction(title: "Share", style: .default, handler: {(alert :UIAlertAction!) in
+            self.shareMasterPiece()
+        })
+        alertController.addAction(shareAction)
+            
+        // these two settings are for iPad devices
+        alertController.popoverPresentationController?.sourceView = self.view
+        alertController.popoverPresentationController?.sourceRect = ((sender.value(forKey: "view") as? UIView)?.frame)!
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func saveMasterPiece() {
+        print("save button tapped")
+    }
+    
+    func shareMasterPiece() {
+        // if saving to photo library, permission must be asked explicitly beforehand
+        print("share button tapped")
+    }
+    
+    func drawALine(first: CGPoint, second: CGPoint) {
+        
+        UIGraphicsBeginImageContext(drawingImageView.bounds.size)
+        
+        let context = UIGraphicsGetCurrentContext()
+        drawingImageView.draw(CGRect(origin: CGPoint(x: 0, y: 0), size: drawingImageView.bounds.size))
+        context?.setStrokeColor(currentColor.cgColor)
+        context?.setLineWidth(currentBrushSize)
+        context?.setLineCap(.round)
+        context?.move(to: first)
+        context?.addLine(to: second)
+        context?.strokePath()
+        drawingImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    func setStrokeColor(color: UIColor) {
+        currentColor = color
+    }
+    
+    
+    
 }
 
